ecbc804d2919324d9e090f0c5b12390bc6b09fc8
Austin Ameh <arewageek@gmail.com>
2025-06-19 13:23:06 +0100

    Refactor image handling: remove unused download_image_to_tempfile function and update DeepFace.verify threshold; specify deepface version in requirements

--- main.py
+++ main.py
@@ -29,29 +29,7 @@ def base64_to_temp_image(base64_str: str, suffix=".jpg") -> str:
     except Exception as e:
         raise ValueError(f"Failed to decode and save base64 image: {e}")
 
-def download_image_to_tempfile(url: str):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
 
-        content_type = response.headers.get("Content-Type", "")
-        print(f"Downloaded content-type: {content_type}")
-        if not content_type.startswith("image/"):
-            raise ValueError(f"URL does not point to a valid image: {url}")
-
-        ext = mimetypes.guess_extension(content_type)
-        if not ext:
-            ext = ".jpg"  # fallback
-
-        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=ext)
-        temp_file.write(response.content)
-        temp_file.close()
-        print(f"Saved image to: {temp_file.name}")
-        return temp_file.name
-
-    except Exception as e:
-        raise ValueError(f"Failed to download or save image from {url}: {e}")
-    
 @api.post("/verify")
 async def verify(data: FaceRequest):
     try:
@@ -60,7 +38,7 @@ async def verify(data: FaceRequest):
 
         print({"ref": ref_img, "test": test_img})
 
-        result = DeepFace.verify(img1_path=ref_img, img2_path=test_img)
+        result = DeepFace.verify(img1_path=ref_img, img2_path=test_img, threshold=0.3)
 
         print({"result": result});
 
